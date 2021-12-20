4987d4f8bb04a4e1c69efd290f504d257112aee7
wilbur61 <93402323+wilbur61@users.noreply.github.com>
2021-12-20 10:04:07 -0500

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,7 +30,8 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random();
+        //double r = Math.random();
+        double r = 777.0;
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
